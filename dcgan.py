a4b4e1c2580e6a3aa6a6bcb50646a8256f02f0f2
muriatec <muriatec2133@gmail.com>
2024-05-28 17:18:30 -0700

    modified:   dcgan.py
    	modified:   dcgan.py

--- dcgan.py
+++ dcgan.py
@@ -10,17 +10,16 @@ class Generator(nn.Module):
             nn.ReLU(inplace=True), # output size: 1024 x 4 x 4
             nn.ConvTranspose2d(in_channels=1024, out_channels=512, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm2d(num_features=512),
-            nn.ReLU(inplace=True), # output size: 512 x 8 x 8,
+            nn.ReLU(inplace=True), # output size: 512 x 8 x 8
             nn.ConvTranspose2d(in_channels=512, out_channels=256, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm2d(num_features=256),
-            nn.ReLU(inplace=True), # output size: 256 x 16 x 16,
+            nn.ReLU(inplace=True), # output size: 256 x 16 x 16
             nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm2d(num_features=128),
-            nn.ReLU(inplace=True), # output size: 128 x 32 x 32,
+            nn.ReLU(inplace=True), # output size: 128 x 32 x 32
             nn.ConvTranspose2d(in_channels=128, out_channels=3, kernel_size=4, stride=2, padding=1),
-            nn.Tanh()
+            nn.Tanh() # output size: 3 x 64 x 64,
         )
         
-
     def forward(self, x):
         return self.main(x)
